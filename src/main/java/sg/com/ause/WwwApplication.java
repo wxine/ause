eb110acbeff570683d60b16aac1792f511b9fc83
Larawei <764098204@qq.com>
2016-10-31 09:11:18 +0800

    sb xjx

---
+++ src/main/java/sg/com/ause/WwwApplication.java
@@ -0,0 +1,12 @@
+package sg.com.ause;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwwApplication.class, args);
+	}
+}
